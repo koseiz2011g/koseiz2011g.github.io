39bc2d22cb033b7e62aeee088f51e8a1582d2d57
koseiz2011g <koseiz2011@gmail.com>
2026-05-27 09:52:36 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,47 @@ const urlsToCache = [
   "/shin4ta/script.js",
   "/shin4ta/manifest.json",
   "/shin4ta/icon-192.png",
-  "/shin4ta/icon-512.png"
+  "/shin4ta/icon-512.png",
+
+  // 画像
+"/shin4ta/images/4c002.png",
+"/shin4ta/images/4cats.png",
+"/shin4ta/images/cat00.jpg",
+"/shin4ta/images/cb01.png",
+"/shin4ta/images/cb02.png",
+"/shin4ta/images/cm_perfect.png",
+"/shin4ta/images/cm01.png",
+"/shin4ta/images/cm02.png",
+"/shin4ta/images/cm03.png",
+"/shin4ta/images/cm04.png",
+"/shin4ta/images/cm05.png",
+"/shin4ta/images/cm06.png",
+"/shin4ta/images/cm07.png",
+"/shin4ta/images/cm08.png",
+"/shin4ta/images/cmi01.png",
+"/shin4ta/images/cmi02.png",
+"/shin4ta/images/cmi03.png",
+"/shin4ta/images/cs01.png",
+"/shin4ta/images/cs02.png",
+"/shin4ta/images/cs03.png",
+"/shin4ta/images/cs03new.png",
+"/shin4ta/images/cs04.png",
+"/shin4ta/images/cw01.png",
+"/shin4ta/images/cw02.png",
+"/shin4ta/images/cw03.png",
+
+// 効果音
+  "/shin4ta/sounds/correct.mp3",
+  "/shin4ta/sounds/wrong.mp3",
+  "/shin4ta/sounds/select.mp3",
+  "/shin4ta/sounds/finish.mp3",
+  "/shin4ta/sounds/next.mp3",
+  "/shin4ta/sounds/tryagain.mp3",
+  "/shin4ta/sounds/result.mp3",
+  "/shin4ta/sounds/milestone.mp3",
+  "/shin4ta/sounds/record.mp3",
+  "/shin4ta/sounds/perfect.mp3",
+  "/shin4ta/sounds/mew01.mp3"
 ];
 
 // インストール
